00de054dbc12b03c6afd31616cd59061d5d4e943
WooJinSeo <74049351+WSC1101@users.noreply.github.com>
2025-11-04 13:30:50 +0900

    Update and rename GrabHandler.java to PinchHandler.java

---
+++ PinchHandler.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class PinchHandler implements GestureHandler {
+
+    @Override
+    public void handle(JSONObject data) {
+
+    }
+
+
+}
